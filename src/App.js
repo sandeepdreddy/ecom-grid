5b2393a1d51081fc3cbeb6f90dd954058fd390e9
Sandeep Duvvur <sandeepkduvvur@gmail.com>
2023-02-14 22:05:09 -0800

    use products data for the layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
+import { mockProductsResponse } from './mockData'
 
 function App() {
+  const { products } = mockProductsResponse
   return (
     <>
       <h1>Hearst E-Commerce</h1>
       <div className='main-container'>
         <div className='grid-container'>
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
-          <div>Item 5</div>
-          <div>Item 6</div>
-          <div>Item 7</div>
-          <div>Item 8</div>
+        {products.map((product) => (
+            <div key={product.product_id} className='grid-item'>
+              <div>{product.title}</div>
+              <div>{product.image}</div>
+            </div>
+          ))}
         </div>
       </div>
     </>
---
+++ src/mockData.js
@@ -0,0 +1,11 @@
+export const mockProductsResponse = {
+    "products": [
+        { "product_id": 1, "url": "/product/1", "title": "Product A", "image": "https://picsum.photos/200" }, 
+        { "product_id": 2, "url": "/product/2", "title": "Gizmo B", "image": "https://picsum.photos/200" }, 
+        { "product_id": 3, "url": "/product/3", "title": "Widget C", "image": "https://picsum.photos/200" }, 
+        { "product_id": 5, "url": "/product/5", "title": "Subscription E", "image": "https://picsum.photos/200" }, 
+        { "product_id": 4, "url": "/product/4", "title": "Magazine D", "image": "https://picsum.photos/200" }, 
+        { "product_id": 6, "url": "/product/6", "title": "Ticket F", "image": "https://picsum.photos/200" }, 
+        { "product_id": 7, "url": "/product/7", "title": "Video G", "image": "https://picsum.photos/200" }
+    ]
+}
